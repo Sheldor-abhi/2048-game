769ae3c1ed7b312da405d9c9f6ae06592e41b1aa
Sheldor-abhi <59531658+Sheldor-abhi@users.noreply.github.com>
2020-01-06 18:17:23 +0530

    Add files via upload

--- game 2048.c
+++ game 2048.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<conio.h>
 char key;
-int a[5][5]={0},i=0,j=0,ir,jr,temp,bu[5]={0};
+int a[5][5]={0},i=0,j=0,ir,jr,temp,bu[5]={0},score=0;
 
 void display()
 {
@@ -17,7 +18,23 @@ void display()
     }
     printf("\n");
     }
+    printf("SCORE= %d \n",score);
     printf("enter W,S,A or D");
+}
+int gameo()
+{
+    int x1,x2,go=0;
+
+    for(x1=0;x1<5;x1++)
+    {
+        for(x2=0;x2<5;x2++)
+        {
+            if(a[x1][x2]==0)
+                go=1;
+        }
+
+    }
+    return go;
 }
 
  void ran1()
@@ -56,16 +73,23 @@ void up()
          }
       }
   }
-  if(arrf())
+  //for(int i=0;i<5;i++)
+    //printf("%d",bu[i]);
+
+        //printf("test");
+        if(arrf())
         {
             for(i=0;i<5;i++)
                 {
                  if(bu[i]==bu[i+1]&&bu[i]!=0)
                  {
                      bu[i]=2*bu[i];
+                     score=score+bu[i];
                      for(j=i+1;j<5;j++)
                      {
+                         //printf("\n%d",bu[j]);
                       bu[j]=bu[j+1];
+                     // printf("\n%d",bu[j]);
 
                      }
                     bu[4]=0;
@@ -75,6 +99,43 @@ void up()
 
         }
 }
+
+/*void down()
+{
+  int temp;
+  for(i=0;i<5;i++)
+  {
+
+      for(j=0;i<5;j++)
+      {
+
+         if(bu[4-j]==0&&bu[4-j-1]!=0)
+         {
+             temp=bu[4-j];
+             bu[4-j]=bu[4-j-1];
+             bu[4-j-1]=temp;
+         }
+      }
+  }
+    if(arrf())
+        {
+         for(i=0;i<5;i++)
+                {
+                 if(bu[4-i]==bu[4-i-1]&&bu[4-i]!=0)
+                 {
+                     bu[4-i]=2*bu[4-i];
+                     for(j=i+1;j<5;j++)
+                     {
+                      bu[4-j]=bu[4-j-1];
+                     }
+                     bu[0]=0;
+
+                 }
+
+                }
+
+        }
+}*/
 void main()
 {
 int x,y;
@@ -83,9 +144,9 @@ int x,y;
     ran1();
     display();
 
-    while(1)
+    while(gameo())
     {
-      key = getchar();
+      key = getch();
 
       if(key == 'w'|| key == 'W')
       {
@@ -94,15 +155,19 @@ int x,y;
           for(y=0;y<5;y++)
           {
               bu[y]=a[y][x];
-
+              //printf("%d",bu[y]);
           }
           up();
            for(y=0;y<5;y++)
           {
               a[y][x]=bu[y];
+              //printf("%d",bu[y]);
           }
+
+
         }
         ran1();
+        system("cls");
       display();
       }
 
@@ -122,7 +187,8 @@ int x,y;
 
         }
         ran1();
-      display();
+        system("cls");
+        display();
       }
 
       else if(key =='a'|| key =='A')
@@ -141,6 +207,7 @@ int x,y;
 
         }
         ran1();
+        system("cls");
       display();
       }
       else if(key == 'd'|| key == 'D')
@@ -159,13 +226,26 @@ int x,y;
 
         }
         ran1();
+        system("cls");
       display();
       }
 
 
     }
+    printf("GAME OVER");
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
